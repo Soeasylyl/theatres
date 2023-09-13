be852b48676d2bcd5198bafd5dd1a495008196c6
sergey_gostev <soeasygos@gmail.com>
2023-09-13 14:21:20 +0300

    added migrations and 2 models

---
+++ app/Models/Hall.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Hall extends Model
+{
+//    use HasFactory;
+    public function cinema() : BelongsTo
+    {
+        return $this->belongsTo(Cinema::class);
+    }
+}
