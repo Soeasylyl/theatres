807ad13c54d1eb1c4c6b193818310d10a9b433f8
sergey_gostev <soeasygos@gmail.com>
2023-09-26 17:18:06 +0300

    fix NameToValueCaster.php

--- app/Casts/NameToValueCaster.php
+++ app/Casts/NameToValueCaster.php
@@ -15,7 +15,7 @@ class NameToValueCaster implements CastsAttributes
     public function get(Model $model, string $key, mixed $value, array $attributes): mixed
     {
         // Return the 'name' attribute value from the attributes array, if it exists.
-        return $attributes['name'] ?? null;
+        return $value;
     }
 
     /**
@@ -25,11 +25,10 @@ class NameToValueCaster implements CastsAttributes
      */
     public function set(Model $model, string $key, mixed $value, array $attributes): mixed
     {
-        // Set the 'name' attribute to the provided value.
-        $attributes['name'] = $value;
-        // If needed, remove the original 'value' attribute.
-        unset($attributes[$key]);
+        if ($value instanceof Enum) {
+            return $value->value;
+        }
 
-        return $attributes;
+        return $value;
     }
 }
