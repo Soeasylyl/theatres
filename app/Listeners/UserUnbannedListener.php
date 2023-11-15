f2a70105f1876a26e5b6fd8c3718b65118d333b4
sergey_gostev <soeasygos@gmail.com>
2023-11-16 00:25:38 +0300

    minor fixed

---
+++ app/Listeners/UserUnbannedListener.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\UserUpdateEvent;
+use App\Mail\UnbanNotificationMail;
+use Illuminate\Support\Facades\Mail;
+
+class UserUnbannedListener
+{
+    /**
+     *
+     * Handles the UserUpdateEvent by checking if the "blocked_until" attribute has been modified to null,
+     *  and if so, sends an UnbanNotificationMail to the user's email address.
+     */
+    public function handle(UserUpdateEvent $event): void
+    {
+        if ($event->user->isDirty('blocked_until') && $event->user->blocked_until === null) {
+                 Mail::to($event->user->email)->send(new UnbanNotificationMail($event->user));
+            }
+    }
+}
