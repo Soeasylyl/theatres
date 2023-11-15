5012d3eacc9cb25dc077729a65eb51c2d20e7b7c
sergey_gostev <soeasygos@gmail.com>
2023-11-15 22:16:04 +0300

    minor fixed

--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -13,7 +13,6 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Pagination\LengthAwarePaginator;
 use Illuminate\Support\Collection;
 
-
 class UserRepository implements UserRepositoryInterface
 {
     /**
