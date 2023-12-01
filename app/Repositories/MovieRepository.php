8d3e68c4fa5a4046b932d9eb2f27ed2779cb4237
sergey_gostev <soeasygos@gmail.com>
2023-12-01 12:26:14 +0300

    minor fixes

--- app/Repositories/MovieRepository.php
+++ app/Repositories/MovieRepository.php
@@ -57,7 +57,7 @@ class MovieRepository implements MovieRepositoryInterface
      * @return Movie The retrieved movie with specified relationships.
      * @throws ModelNotFoundException If the movie with the given ID is not found.
      */
-    public function getMovieWithRelationsOrFail(int $movieId, ?array $relations = []): Movie
+    public function getMovieByIdOrFail(int $movieId, ?array $relations = []): Movie
     {
         return Movie::with($relations)->findOrFail($movieId);
     }
