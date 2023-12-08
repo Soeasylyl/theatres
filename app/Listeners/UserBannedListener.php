80f8fe34d070c062c714d6d398961abe7ab847a0
sergey_gostev <soeasygos@gmail.com>
2023-12-08 11:21:25 +0300

    fixes job

--- app/Listeners/UserBannedListener.php
+++ app/Listeners/UserBannedListener.php
@@ -27,11 +27,14 @@ class UserBannedListener implements ShouldQueue
     /**
      * Handle a job failure.
      *
+     * @param UserUpdateEvent $event
      * @param \Throwable $exception
      * @return void
      */
-    public function failed(\Throwable $exception): void
+    public function failed(UserUpdateEvent $event, \Throwable $exception): void
     {
-        Log::error($exception->getMessage());
+        Log::error('Failed to send ban notification email for User ID: ' . $event->user->id
+            . 'Message:' . $exception->getMessage()
+        );
     }
 }
