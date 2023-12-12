5b81c3ec4a4680ce2a9b40ae8e90fc46aca624b7
sergey_gostev <soeasygos@gmail.com>
2023-12-12 21:03:51 +0300

    test

---
+++ resources/js/admin/pages/hallCanvas.js
@@ -0,0 +1,20 @@
+class HallCanvas {
+    constructor() {
+        this.adpCanvas = document.getElementById("hallCanvas");
+        this.adpCtx = this.adpCanvas.getContext("2d");
+
+        this.init();
+    }
+
+    init() {
+        this.generateHall();
+    }
+
+    generateHall() {
+
+
+
+    }
+}
+
+new HallCanvas();
